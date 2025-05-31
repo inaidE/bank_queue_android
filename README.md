496e172e162e6a7827e2968d359e729f1a85326e
spartanec22832 <b.bogovski@yandex.ru>
2025-05-31 23:34:11 +0300

    Create the project with basic README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android Client (BankQueue)
+
+Мобильное приложение для Android (Kotlin + Jetpack Compose).
+
+## Открытие в Android Studio
+
+1. Склонируйте репозиторий:
+```bash
+git clone https://github.com/inaidE/bank_queue_android.git
+```
---
+++ app/src/main/java/com/sfedu/bank_queue_android/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sfedu.bank_queue_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sfedu.bank_queue_android.ui.theme.Bank_queue_androidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Bank_queue_androidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Bank_queue_androidTheme {
+        Greeting("Android")
+    }
+}
